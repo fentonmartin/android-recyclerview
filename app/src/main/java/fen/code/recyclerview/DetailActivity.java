a3b743790dfa4e6c8c60500f4088720ecfc1e4d0
Fenton Martin <fen.official@gmail.com>
2017-06-06 19:26:04 +0700

    Add TextView on Detail Activity

--- app/src/main/java/fen/code/recyclerview/DetailActivity.java
+++ app/src/main/java/fen/code/recyclerview/DetailActivity.java
@@ -2,6 +2,7 @@ package fen.code.recyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        TextView textView = (TextView) findViewById(R.id.detail_text);
     }
 }
