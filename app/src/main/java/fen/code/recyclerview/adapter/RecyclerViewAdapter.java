6b67744b4c6f8a2e99db8bc8f57ef70b7653772c
Fenton Martin <fen.official@gmail.com>
2017-06-06 19:25:34 +0700

    Add OnClickListener on ImageView

--- app/src/main/java/fen/code/recyclerview/adapter/RecyclerViewAdapter.java
+++ app/src/main/java/fen/code/recyclerview/adapter/RecyclerViewAdapter.java
@@ -1,6 +1,7 @@
 package fen.code.recyclerview.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -11,6 +12,7 @@ import android.widget.TextView;
 
 import java.util.List;
 
+import fen.code.recyclerview.DetailActivity;
 import fen.code.recyclerview.R;
 import fen.code.recyclerview.entity.ViewItem;
 
@@ -48,14 +50,28 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     class RecyclerViewHolder extends RecyclerView.ViewHolder {
 
+        Context context;
         TextView textView;
         ImageView imageView;
 
         RecyclerViewHolder(View view) {
             super(view);
 
+            /* getContext from view */
+            context = view.getContext();
+
+            /* initiate item from view */
             textView = (TextView) view.findViewById(R.id.item_text);
             imageView = (ImageView) view.findViewById(R.id.item_image);
+
+            /* setOnClickListener on imageView */
+            imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(v.getContext(), DetailActivity.class);
+                    context.startActivity(intent);
+                }
+            });
         }
     }
 
